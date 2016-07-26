d2007e1d46c3a1beefccf98386009e976e7d8d2f
Nick-Getka <ngetka@new-host.home>
2016-07-26 16:54:00 -0400

    First Test Commit

---
+++ src/main/java/Imager_Project/imager/App.java
@@ -0,0 +1,13 @@
+package Imager_Project.imager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
